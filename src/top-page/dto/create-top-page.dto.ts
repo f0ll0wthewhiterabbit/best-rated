5fd38aedf354ba8e3fd095957b971365c126bfd7
f0ll0wthewhiterabbit <3tonishere@gmail.com>
2023-10-29 17:57:18 +0300

    Implemented TopPageController
    Attached missing auth guards

---
+++ src/top-page/dto/create-top-page.dto.ts
@@ -0,0 +1,62 @@
+import { Type } from 'class-transformer';
+import { IsArray, IsEnum, IsNumber, IsOptional, IsString, ValidateNested } from 'class-validator';
+import { TopLevelCategory } from '../top-page.model';
+
+export class HHDataDto {
+  @IsNumber()
+  count: number;
+
+  @IsNumber()
+  juniorSalary: number;
+
+  @IsNumber()
+  middleSalary: number;
+
+  @IsNumber()
+  seniorSalary: number;
+}
+
+export class TopPageAdvantagesDto {
+  @IsString()
+  title: string;
+
+  @IsString()
+  description: string;
+}
+
+export class CreateTopPageDto {
+  @IsEnum(TopLevelCategory)
+  firstCategory: TopLevelCategory;
+
+  @IsString()
+  secondCategory: string;
+
+  @IsString()
+  alias: string;
+
+  @IsString()
+  title: string;
+
+  @IsString()
+  category: string;
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => HHDataDto)
+  hh?: HHDataDto;
+
+  @IsArray()
+  @ValidateNested()
+  @Type(() => TopPageAdvantagesDto)
+  advantages: TopPageAdvantagesDto[];
+
+  @IsString()
+  seoText: string;
+
+  @IsString()
+  tagsTitle: string;
+
+  @IsArray()
+  @IsString({ each: true })
+  tags: string[];
+}
